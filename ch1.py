dee1f54bcfa7e8a7a051a3c10f4ce2e8fbbc8499
Polowis <39208974+polowis@users.noreply.github.com>
2019-08-05 10:50:38 +1000

    Create ch1.py

---
+++ ch1.py
@@ -0,0 +1,39 @@
+import math
+class Point:
+
+    def __init__(self, initX, initY):
+        """ Create a new point at the given coordinates. """
+        self.x = initX
+        self.y = initY
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def distanceFromOrigin(self):
+        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
+
+    def __str__(self):
+        return "x=" + str(self.x) + ", y=" + str(self.y)
+
+    def halfway(self, target):
+         mx = (self.x + target.x) / 2
+         my = (self.y + target.y) / 2
+         return Point(mx, my)
+        
+    def distanceFromPoint(self, point):
+        newx = (point.x - self.x)
+        newy = (point.y - self.y)
+        return math.sqrt(newx ** 2 + newy ** 2)
+
+p = Point(3, 4)
+q = Point(5, 12)
+mid = p.halfway(q)
+
+print(mid)
+print(mid.getX())
+print(mid.getY())
+print(p.distanceFromPoint(q))
+
